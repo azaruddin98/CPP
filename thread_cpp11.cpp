c29ecca44e1943ee9673dbc08bad248db784920d
Shaikh Azaruddin <azaruddin98@gmail.com>
2025-07-27 22:43:06 +0530

    Update thread_cpp11.cpp

--- thread_cpp11.cpp
+++ thread_cpp11.cpp
@@ -1,3 +1,8 @@
+/*Multithreading is a technique where a program is divided into smaller units of execution called threads.
+Each thread runs independently but shares resources like memory, allowing tasks to be performed simultaneously. 
+This helps improve performance by utilizing multiple CPU cores efficiently. 
+Multithreading support was introduced in C++11 with the introduction of <thread> header file*/
+
 #include<iostream>
 #include<thread>
 #include<mutex>
@@ -8,14 +13,19 @@ mutex objMut;
 void increment(){
     for(int i=0;i<100000;i++)
     {
+        /*In C++, lock_guard is a simple class that is used to manage the locking and unlocking of a mutex.
+        Its main purpose is to automatically lock a mutex when it is created and automatically unlock it when the 
+        lock_guard object goes out of scope. Following is the syntax to use lock_guard in C++*/
+        
          lock_guard<mutex> lock(objMut);
             ++count;
     }
 }
 int main(){
-
+        
         thread t1(increment);
         thread t2(increment);
+        /*It ensures that the calling thread waits for the specified thread to complete its execution.*/
         t1.join();
         t2.join();
 
